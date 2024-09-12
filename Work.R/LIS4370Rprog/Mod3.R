e887cd65fe016e0b49a45c5a192ef198ccad1c4c
DanielDataGit <dtafmizi04@gmail.com>
2024-09-12 12:24:20 -0400

    Add files via upload

---
+++ Work.R/LIS4370Rprog/Mod3.R
@@ -0,0 +1,11 @@
+#create vectors to fill dataframe
+
+Name <- c("Jeb", "Donald", "Ted", "Marco", "Carly", "Hillary", "Berine")
+
+ABC <- c(4, 62, 51, 21, 2, 14, 15)
+
+CBS  <- c(12, 75, 43, 19, 1, 21, 19) 
+
+# fill df with vectors
+
+df <- data.frame(Name, ABC, CBS)
