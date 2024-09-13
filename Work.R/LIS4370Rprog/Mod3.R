eb40a46d85b7c589e2a0b331102812233d286b98
DanielDataGit <dtafmizi04@gmail.com>
2024-09-13 13:49:14 -0400

    Add files via upload

--- Work.R/LIS4370Rprog/Mod3.R
+++ Work.R/LIS4370Rprog/Mod3.R
@@ -1,3 +1,5 @@
+library(ggplot2)
+
 #create vectors to fill dataframe
 
 Name <- c("Jeb", "Donald", "Ted", "Marco", "Carly", "Hillary", "Berine")
@@ -8,4 +10,10 @@ CBS  <- c(12, 75, 43, 19, 1, 21, 19)
 
 # fill df with vectors
 
-df <- data.frame(Name, ABC, CBS)
+df <- data.frame(Name, ABC, CBS, ABC+CBS)
+df <- df[order(-df$ABC...CBS), ]
+
+
+ggplot(df, aes(x = reorder(Name, -ABC...CBS), y = ABC...CBS)) +
+  geom_col() 
+  
