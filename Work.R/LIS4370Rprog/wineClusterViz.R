3c5541a68826877cc1514fe7443472641b1f101c
DanielDataGit <dtafmizi04@gmail.com>
2024-10-24 06:59:08 -0400

    Update wineClusterViz.R

--- Work.R/LIS4370Rprog/wineClusterViz.R
+++ Work.R/LIS4370Rprog/wineClusterViz.R
@@ -7,32 +7,33 @@ library(pheatmap)
 install.packages("ggplotify")
 library(ggplotify)
 
+
 ogdata <- read.csv("week9\\wine.csv")
 
+#prep data for kmeans vis
 x <- ogdata$country
-
 data <- scale(ogdata[, 3:6])
-
 row.names(data) <- x
 
+#plot raw data
+pairs(data)
+
+#calculate kmeans
 km <- kmeans(data, centers = 3, nstart = 5)
 
+#plot cluster vis
 fviz_cluster(km, data = data)
 
-
+#prep data for heatmap
 numdata <- ogdata[, 3:6]
-
 normalized_df <- as.data.frame(lapply(numdata, function(x) {
   (x - min(x)) / (max(x) - min(x))
 }))
-
 row.names(normalized_df) <- x
 
+#plot clustered heatmap
 hm <- as.ggplot(pheatmap(normalized_df))
 
 hm +
   labs(title = "Heatmap of Wine Data")
-
-
-
 
